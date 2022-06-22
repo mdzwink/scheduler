682bf81ee7ef9e7df787434643ecc451198f3b8b
mdzwink <mdzwink@gmail.com>
2022-06-22 11:49:56 -0400

    refactored to move logic into useApplicationData.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,45 +4,20 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index.js"
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors.js"
+import useApplicationData from "../hooks/useApplicationData"
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-  const setDay = (day) => setState({...state, day});
+
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+  console.log(state.days)
   const appointmentsArr = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day)
 
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
-     .then(() => setState({...state, appointments}))
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-      .then(() => setState({...state, appointments}))
-  }
-
-  
   const schedule = appointmentsArr.map(appointment => {
     const interview = getInterview(state, appointment.interview);
     return <Appointment
@@ -54,18 +29,7 @@ export default function Application(props) {
     bookInterview={bookInterview}
     onDelete={cancelInterview}
     />
-});
-  
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-    })
-    .catch((err) => console.log('ERROR FROM useEffect>>>', err))
-  }, []);
+  });
 
 
   return (
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  const setDay = (day) => setState({...state, day});
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+     .then(() => setState({...state, appointments}))
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => setState({...state, appointments}))
+  }
+
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+    })
+    .catch((err) => console.log('ERROR FROM useEffect>>>', err))
+  }, []);
+
+  return { state, setDay, bookInterview, cancelInterview}
+}
+
+// The state object will maintain the same structure.
+// The setDay action can be used to set the current day.
+// The bookInterview action makes an HTTP request and updates the local state.
+// The cancelInterview action makes an HTTP request and updates the local state.
+// Summary
+// return {state: ..,setDay: .., bookInterview: .., cancelInterview: ..}
