7b2e05411808a18b6fb2ec85e5164a69f57ca1c4
mdzwink <mdzwink@gmail.com>
2022-06-24 00:42:56 -0400

    add useApplicationData.js with primary logic for application

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
@@ -8,8 +8,29 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
+
   const setDay = (day) => setState({...state, day});
 
+  // returns new Array of day Objects with updated spots count. Requires state and appointments Array with updated interview data.
+  const updateSpots = function(state, appointments) {
+    let spotsCount = 0;
+    let theDay = state.days.find(day => day.name === state.day);
+    let newDay = {...theDay};
+    let newDaysArr = [...state.days];
+    let newDayIndex = newDaysArr.findIndex((day) => day.name === state.day);
+    
+    for (let id of newDay.appointments) {
+      if (!appointments[id].interview) {
+        spotsCount += 1;
+      }
+    }
+    newDay.spots = spotsCount;
+    newDaysArr.splice(newDayIndex, 1, newDay);
+
+    return newDaysArr;
+  };
+
+  // returns promise from axios call. Updates api database with new appointment given appointment id and interview object.
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -19,10 +40,13 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+    const days = updateSpots(state, appointments);
+
     return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
-     .then(() => setState({...state, appointments}))
+      .then(() => setState({...state, days, appointments}));
   }
 
+  // returns promise from axios call. Updates appointment Object in api database. Sets assosiated interview value to null. Requires appointment id.
   function cancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
@@ -32,11 +56,13 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+    const days = updateSpots(state, appointments);
+
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-      .then(() => setState({...state, appointments}))
+      .then(() => setState({...state, days, appointments}))
   }
 
-  
+  // initial axios calls to api database on refresh
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
@@ -48,12 +74,5 @@ export default function useApplicationData() {
     .catch((err) => console.log('ERROR FROM useEffect>>>', err))
   }, []);
 
-  return { state, setDay, bookInterview, cancelInterview}
+  return { state, setDay, bookInterview, cancelInterview};
 }
-
-// The state object will maintain the same structure.
-// The setDay action can be used to set the current day.
-// The bookInterview action makes an HTTP request and updates the local state.
-// The cancelInterview action makes an HTTP request and updates the local state.
-// Summary
-// return {state: ..,setDay: .., bookInterview: .., cancelInterview: ..}
