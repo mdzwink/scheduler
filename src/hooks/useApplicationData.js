149a3087174f6b31e6350297d4a5deb554f4ade2
mdzwink <mdzwink@gmail.com>
2022-07-04 20:41:50 -0400

    improve syntax and layout

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,16 +8,27 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
-
   const setDay = (day) => setState({...state, day});
 
+  // initial axios calls to api database on refresh
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+    })
+    .catch((err) => console.log('ERROR FROM useEffect>>>', err))
+  }, []);
+  
   // returns new Array of day Objects with updated spots count. Requires state and appointments Array with updated interview data.
   const updateSpots = function(state, appointments) {
     let spotsCount = 0;
-    let theDay = state.days.find(day => day.name === state.day);
-    let newDay = {...theDay};
-    let newDaysArr = [...state.days];
-    let newDayIndex = newDaysArr.findIndex((day) => day.name === state.day);
+    const theDay = state.days.find(day => day.name === state.day);
+    const newDay = {...theDay};
+    const newDaysArr = [...state.days];
+    const newDayIndex = newDaysArr.findIndex((day) => day.name === state.day);
     
     for (let id of newDay.appointments) {
       if (!appointments[id].interview) {
@@ -61,18 +72,7 @@ export default function useApplicationData() {
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
       .then(() => setState({...state, days, appointments}))
   }
-
-  // initial axios calls to api database on refresh
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-    })
-    .catch((err) => console.log('ERROR FROM useEffect>>>', err))
-  }, []);
+  
 
   return { state, setDay, bookInterview, cancelInterview};
 }
